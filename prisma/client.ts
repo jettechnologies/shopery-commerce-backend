e346ecaa8ecbb8b54ab09e26b9df13532bf4fbd3
jettechnologies <tomesitime04@gmail.com>
2026-02-09 06:54:22 -0800

    feat: updated render.yaml file to reflect the use of neon db

--- prisma/client.ts
+++ prisma/client.ts
@@ -2,6 +2,12 @@ import "dotenv/config";
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
+const NODE_ENV = process.env.NODE_ENV;
+const IS_DEV = NODE_ENV === "development";
+const connectionString = IS_DEV
+  ? process.env.LOCAL_DATABASE_URL
+  : process.env.PROD_DATABASE_URL;
+
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
