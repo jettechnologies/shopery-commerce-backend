890b7b2315e98c41ea4fd57481c4796b6d71d567
jettechnologies <tomesitime04@gmail.com>
2026-04-17 10:24:01 -0700

    refactor: improved on the creation of search_vector to a determinsitc output

--- prisma/migrations/20260417054136_add_search_vector/migration.sql
+++ prisma/migrations/20260417054136_add_search_vector/migration.sql
@@ -1,30 +1,12 @@
--- Add column
 ALTER TABLE "Product"
-ADD COLUMN search_vector tsvector;
+DROP COLUMN IF EXISTS search_vector;
 
--- Populate existing rows
-UPDATE "Product"
-SET search_vector =
-  setweight(to_tsvector('english', name), 'A') ||
-  setweight(to_tsvector('english', coalesce(description, '')), 'B');
+ALTER TABLE "Product"
+ADD COLUMN search_vector tsvector GENERATED ALWAYS AS (
+  setweight(to_tsvector('english', coalesce(name, '')), 'A') ||
+  setweight(to_tsvector('english', coalesce(description, '')), 'B')
+) STORED;
 
--- Create index
 CREATE INDEX product_search_idx
 ON "Product"
 USING GIN (search_vector);
-
--- Function to keep it updated
-CREATE FUNCTION product_search_vector_update() RETURNS trigger AS $$
-BEGIN
-  NEW.search_vector :=
-    setweight(to_tsvector('english', NEW.name), 'A') ||
-    setweight(to_tsvector('english', coalesce(NEW.description, '')), 'B');
-  RETURN NEW;
-END
-$$ LANGUAGE plpgsql;
-
--- Trigger
-CREATE TRIGGER product_search_vector_trigger
-BEFORE INSERT OR UPDATE ON "Product"
-FOR EACH ROW
-EXECUTE FUNCTION product_search_vector_update();
